90a269070d5aed237258b3457d8cf178fb5805ec
shikha <shikhatomar94@gmail.com>
2024-06-16 23:55:06 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Header from "./components/Header";
+// import Header from "./components/Header";
+import Navbar from './components/Navbar';
 // import Footer from "./components/Footer";
 
 function App() {
   return  (
     <div>
-      <Header />
+      {/* <Header /> */}
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
